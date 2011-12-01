3d0ad7c25351f68b65462baa12dc914b520b0fa5
Peter Law <PeterJCLaw@gmail.com>
2011-12-01 22:21:33 +0000

    Add a little helper that shows take-home for a given income.

---
+++ take-home.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+from tax import taxFromEarnings
+from rates import national_insurance, income_tax
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print 'Usage: ni EARNINGS'
+		exit(1)
+	earnings = int(sys.argv[1])
+	tax = taxFromEarnings(income_tax, earnings)
+	ni = taxFromEarnings(national_insurance, earnings)
+	print earnings - tax - ni
