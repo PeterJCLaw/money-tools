a80b1f7d485336f74f1fd1e90e06fa01f3d9ccc4
Peter Law <PeterJCLaw@gmail.com>
2022-03-26 11:37:11 +0000

    Modernise more

--- rates.py
+++ rates.py
@@ -1,17 +1,14 @@
-
-import sys
-
 INCOME_TAX = {
     12570: 0,
     50270: 0.2,
     150000: 0.4,
-    sys.maxint: 0.5,
+    float('inf'): 0.5,
 }
 
 NATIONAL_INSURANCE = {
     9564: 0,
     50268: 0.12,
-    sys.maxint: 0.02,
+    float('inf'): 0.02,
 }
 
 def ordered_rates(rates):
