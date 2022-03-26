PeterJCLaw/money-tools

commit 2f8f3429bcaec61f4eee8a581b958c86ddda7e77

Modernise

rates.py
@@ -1,23 +1,18 @@
 
 import sys
 
-income_tax = {
+INCOME_TAX = {
     7475: 0,
     35000: 0.2,
     150000: 0.4,
     sys.maxint: 0.5,
 }
 
-national_insurance = {
+NATIONAL_INSURANCE = {
     7228: 0,
     42484: 0.12,
     sys.maxint: 0.02,
 }
 
-def orderedRates(rates):
-    keys = rates.keys()
-    keys.sort()
-
-    for level in keys:
-        rate = rates[level]
-        yield rate, level
+def ordered_rates(rates):
+    return sorted(rates.items())

earnings.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 import sys
-from rates import orderedRates, income_tax
+from rates import ordered_rates, INCOME_TAX
 
-def earningsFromTax(rates, tax):
+def earnings_from_tax(rates, tax):
     if tax == 0:
-        return "Up to %s" % rates[0]
+        return f"Up to {rates[0]}"
 
     earn = 0
 
-    for (rate, level) in orderedRates(rates):
+    for (rate, level) in ordered_rates(rates):
         max_tax = level * rate	# at this band
 
 #		print rate, level, max_tax, tax
@@ -25,6 +25,6 @@ def earningsFromTax(rates, tax):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print 'Usage: earnings TAX'
+        print("Usage: earnings TAX")
         exit(1)
-    print earningsFromTax(income_tax, int(sys.argv[1]))
+    print(earnings_from_tax(INCOME_TAX, int(sys.argv[1])))

ni.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import sys
-from tax import taxFromEarnings
-from rates import national_insurance
+from tax import tax_from_earnings
+from rates import NATIONAL_INSURANCE
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print 'Usage: ni EARNINGS'
+        print("Usage: ni EARNINGS")
         exit(1)
-    print taxFromEarnings(national_insurance, int(sys.argv[1]))
+    print(tax_from_earnings(NATIONAL_INSURANCE, int(sys.argv[1])))

take-home.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-from tax import taxFromEarnings
-from rates import national_insurance, income_tax
+from tax import tax_from_earnings
+from rates import NATIONAL_INSURANCE, INCOME_TAX
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print 'Usage: ni EARNINGS'
+        print("Usage: ni EARNINGS")
         exit(1)
     earnings = int(sys.argv[1])
-    tax = taxFromEarnings(income_tax, earnings)
-    ni = taxFromEarnings(national_insurance, earnings)
-    print earnings - tax - ni
+    tax = tax_from_earnings(INCOME_TAX, earnings)
+    ni = tax_from_earnings(NATIONAL_INSURANCE, earnings)
+    print(earnings - tax - ni)

tax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-from rates import income_tax, orderedRates
+from rates import INCOME_TAX, ordered_rates
 
-def taxFromEarnings(rates, earn):
+def tax_from_earnings(rates, earn):
     tax = 0
 
-    for (rate, level) in orderedRates(rates):
+    for (rate, level) in ordered_rates(rates):
         left = earn - level
         if left > 0:
             earn = left
@@ -19,6 +19,6 @@ def taxFromEarnings(rates, earn):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print 'Usage: tax EARNINGS'
+        print("Usage: tax EARNINGS")
         exit(1)
-    print taxFromEarnings(income_tax, int(sys.argv[1]))
+    print(tax_from_earnings(INCOME_TAX, int(sys.argv[1])))

commit 653ad35411fd8393c19054ae7add825d08729b2d

Fix rates ordering logic
2f8f342 accidentally broke this as I missed that it yields tuples
of (rate, level) rather than (level, rate).

rates.py
@@ -12,4 +12,5 @@ NATIONAL_INSURANCE = {
 }
 
 def ordered_rates(rates):
-    return sorted(rates.items())
+    for level in sorted(rates.keys()):
+        yield rates[level], level