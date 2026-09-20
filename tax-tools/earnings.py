3320147a017e3bb42240daa6aa545751bcf1d41d
Peter Law <PeterJCLaw@gmail.com>
2026-09-20 10:09:58 +0100

    Explicitly python3

--- tax-tools/earnings.py
+++ tax-tools/earnings.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 from rates import ordered_rates, INCOME_TAX
--- tax-tools/tax.py
+++ tax-tools/tax.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 from rates import INCOME_TAX, ordered_rates
